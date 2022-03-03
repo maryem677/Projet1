681f8b5bf5a34b5c89a0c7c659d62314ad83fec2
Maryem <hamdimaryem96@gmail.com>
2022-03-03 18:42:53 +0100

    stagiaireFinal

--- src/model/Stagiaire.java
+++ src/model/Stagiaire.java
@@ -1,6 +1,5 @@
 package model;
 
-
 public class Stagiaire {
     private String nom;
     private String prenom;
@@ -92,58 +91,35 @@ public class Stagiaire {
 		return "Stagiaire [nom=" + nom + ", prenom=" + prenom + ", departement=" + departement + ", promo=" + promo
 				+ ", annee=" + annee + "]";
 	}
-
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + annee;
-		result = prime * result + ((departement == null) ? 0 : departement.hashCode());
-		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
-		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
-		result = prime * result + ((promo == null) ? 0 : promo.hashCode());
-		return result;
-	}
-
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Stagiaire other = (Stagiaire) obj;
-		if (annee != other.annee)
-			return false;
-		if (departement == null) {
-			if (other.departement != null)
-				return false;
-		} else if (!departement.equals(other.departement))
-			return false;
-		if (nom == null) {
-			if (other.nom != null)
-				return false;
-		} else if (!nom.equals(other.nom))
-			return false;
-		if (prenom == null) {
-			if (other.prenom != null)
-				return false;
-		} else if (!prenom.equals(other.prenom))
-			return false;
-		if (promo == null) {
-			if (other.promo != null)
-				return false;
-		} else if (!promo.equals(other.promo))
-			return false;
-		return true;
-	}
     
+    public Boolean compare(Stagiaire stagiaireAComparer) {
+    	Boolean b = false;
+    	if (this.nom.equals(stagiaireAComparer.nom) && this.prenom.equals(stagiaireAComparer.prenom) && this.promo.equals(stagiaireAComparer.promo) && this.annee==stagiaireAComparer.annee) {
+    		b = true;
+    	}
+    	
+    	return b;
+    }
     
+    public Boolean compareMultiCrit(Stagiaire stagiaireAComparer) {
+    	Boolean b = true;
+    	if ((!stagiaireAComparer.nom.equals("")) && (!this.nom.equalsIgnoreCase(stagiaireAComparer.nom))) {
+    		b = false;
+    	}
+    	if ((!stagiaireAComparer.prenom.equals("")) && (!this.prenom.equalsIgnoreCase(stagiaireAComparer.prenom))) {
+    		b = false;
+    	}
+    	if ((!stagiaireAComparer.departement.equals("")) && (!this.departement.equalsIgnoreCase(stagiaireAComparer.departement))) {
+    		b = false;
+    	}
+    	if ((!stagiaireAComparer.promo.equals("")) && (!this.promo.equalsIgnoreCase(stagiaireAComparer.promo))) {
+    		b = false;
+    	}
+    	if ((stagiaireAComparer.annee!=0) && (this.annee!=stagiaireAComparer.annee)) {
+    		b = false;
+    	}
+    	return b;
+    }
 
     
     
